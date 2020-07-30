09dd3a4bb5b8c3c222ad43c2cf89d97aa5706ef4
ambikads <ambikads@users.noreply.github.com>
2020-07-30 10:35:31 -0400

    DEPLOY_SCRIPT: Add docker tags and ecs service update

--- deploy.py
+++ deploy.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+import boto3
 import argparse
 from zygoat.utils.files import repository_root, use_dir
 from zygoat.utils.shell import run
@@ -10,8 +11,18 @@ from zygoat.config import Config
 ECR_REPO_PREFIX = "818831340115.dkr.ecr.us-east-1.amazonaws.com"
 
 
+def get_git_commit_hash():
+    return (
+        run(["git", "rev-parse", "--short", "HEAD"], capture_output=True)
+        .stdout.decode()
+        .strip()
+    )
+
+
 def main(args):
     config = Config()
+    ecs = boto3.client("ecs")
+
     with repository_root():
         with use_dir(Projects.BACKEND):
             log.info(f"Updating {args.environment} backend")
@@ -24,16 +35,42 @@ def main(args):
             log.info("Building frontend")
             image_name = f"{config.name}-frontend"
             latest = f"{image_name}:latest"
-            run(["docker", "build", "-t", f"{image_name}", "."])
+            git_commit_hash = get_git_commit_hash()
+            tags = ["latest", f"{args.environment}", f"{git_commit_hash}"]
+
             run(
                 [
                     "docker",
-                    "tag",
-                    f"{config.name}-frontend:latest",
-                    f"{ECR_REPO_PREFIX}/{latest}",
+                    "build",
+                    "-t",
+                    f"{latest}",
+                    "-t",
+                    f"{image_name}:{args.environment}",
+                    "-t",
+                    f"{image_name}:{git_commit_hash}",
+                    ".",
                 ]
             )
-            run(["docker", "push", f"{ECR_REPO_PREFIX}/{latest}"])
+
+            for tag in tags:
+                run(
+                    [
+                        "docker",
+                        "tag",
+                        f"{image_name}:{tag}",
+                        f"{ECR_REPO_PREFIX}/{image_name}:{tag}",
+                    ]
+                )
+                run(["docker", "push", f"{ECR_REPO_PREFIX}/{image_name}:{tag}"])
+
+            log.info(
+                f"Updating service frontend-{args.environment} on {config.name} ECS cluster"
+            )
+            ecs.update_service(
+                cluster=f"{config.name}",
+                service=f"frontend-{args.environment}",
+                forceNewDeployment=True,
+            )
 
 
 if __name__ == "__main__":

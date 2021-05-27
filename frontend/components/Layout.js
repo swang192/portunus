2ef2d2306a3823127369b1c01374b3830914f1f8
Kris Borer <kris@willing.com>
2021-05-27 13:46:16 -0400

    FRONTEND: show upwise logo when appropriate

--- frontend/components/Layout.js
+++ frontend/components/Layout.js
@@ -1,29 +1,43 @@
 import { observer } from 'mobx-react';
 import PropTypes from 'prop-types';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
 
 import Grid from '@wui/layout/grid';
 import Spacer from '@wui/layout/spacer';
 import Panel from '@wui/layout/panel';
 
-const Layout = ({ showLogo, children, slim }) => (
-  <div>
-    <Grid container direction="column" alignItems="center" wrap="nowrap">
-      <Spacer v={32} />
-      <Spacer v={60} xsDown />
-      {showLogo && (
-        <Link href="/">
-          <a aria-label="Home">
-            <img src="/images/mlp-logo.svg" alt="Home" />
-          </a>
-        </Link>
-      )}
-      <Spacer v={32} />
-      <Panel lessPadding={slim}>{children}</Panel>
-      <Spacer v={92} />
-    </Grid>
-  </div>
-);
+const Layout = ({ showLogo, children, slim }) => {
+  const router = useRouter();
+  const [logo, setLogo] = useState('mlp-logo.svg');
+
+  useEffect(() => {
+    // upwise is a MetLife mobile application
+    if (router.asPath.includes('upwise')) {
+      setLogo('upwise-logo.png');
+    }
+  }, [router.asPath]);
+
+  return (
+    <div>
+      <Grid container direction="column" alignItems="center" wrap="nowrap">
+        <Spacer v={32} />
+        <Spacer v={60} xsDown />
+        {showLogo && (
+          <Link href="/">
+            <a aria-label="Home">
+              <img src={`/images/${logo}`} alt="Home" />
+            </a>
+          </Link>
+        )}
+        <Spacer v={32} />
+        <Panel lessPadding={slim}>{children}</Panel>
+        <Spacer v={92} />
+      </Grid>
+    </div>
+  );
+};
 
 Layout.propTypes = {
   showLogo: PropTypes.bool,

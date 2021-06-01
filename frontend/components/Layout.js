d35102c98af43c7a31216c9fea7a50cab7551f46
Kris Borer <kris@willing.com>
2021-06-01 13:13:23 -0400

    UPWISE: add welcome text for upwise users

--- frontend/components/Layout.js
+++ frontend/components/Layout.js
@@ -4,6 +4,9 @@ import Link from 'next/link';
 import { useRouter } from 'next/router';
 import { useEffect, useState } from 'react';
 
+import Container from '@material-ui/core/Container';
+import Typography from '@material-ui/core/Typography';
+
 import Grid from '@wui/layout/grid';
 import Spacer from '@wui/layout/spacer';
 import Panel from '@wui/layout/panel';
@@ -11,11 +14,13 @@ import Panel from '@wui/layout/panel';
 const Layout = ({ showLogo, children, slim }) => {
   const router = useRouter();
   const [logo, setLogo] = useState('mlp-logo.svg');
+  const [showWelcome, setShowWelcome] = useState(false);
 
   useEffect(() => {
     // upwise is a MetLife mobile application
     if (router.asPath.includes('upwise')) {
       setLogo('upwise-logo.png');
+      setShowWelcome(true);
     }
   }, [router.asPath]);
 
@@ -25,11 +30,26 @@ const Layout = ({ showLogo, children, slim }) => {
         <Spacer v={32} />
         <Spacer v={60} xsDown />
         {showLogo && (
-          <Link href="/">
-            <a aria-label="Home">
-              <img src={`/images/${logo}`} alt="Home" />
-            </a>
-          </Link>
+          <>
+            <Link href="/">
+              <a aria-label="Home">
+                <img src={`/images/${logo}`} alt="Home" />
+              </a>
+            </Link>
+            {showWelcome && (
+              <Container maxWidth="sm">
+                <Spacer v={32} />
+                <Typography variant="h6">
+                  Welcome! You are about to start a simple, self-guided process to complete your
+                  estate plan. This can take as little as 15 minutes.
+                </Typography>
+                <Spacer v={12} />
+                <Typography variant="h6">
+                  Let’s get started by creating a secure MetLife Legal Plans account.
+                </Typography>
+              </Container>
+            )}
+          </>
         )}
         <Spacer v={32} />
         <Panel lessPadding={slim}>{children}</Panel>

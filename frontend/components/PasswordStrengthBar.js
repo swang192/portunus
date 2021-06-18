cda1fbb825a253ae24e14763ec522222b6562a67
Shirley Wang <swang192@gmail.com>
2021-06-17 22:34:49 -0400

    A11Y: increase password strength bar color contrast

--- frontend/components/PasswordStrengthBar.js
+++ frontend/components/PasswordStrengthBar.js
@@ -1,6 +1,7 @@
 import { observer } from 'mobx-react';
 import PropTypes from 'prop-types';
 import { useTheme } from '@material-ui/core/styles';
+import { makeStyles } from '@material-ui/core/styles';
 import ExternalPasswordStrengthBar from 'react-password-strength-bar';
 
 import { MIN_PASSWORD_LENGTH } from 'utils/constants';
@@ -8,16 +9,31 @@ import { MIN_PASSWORD_LENGTH } from 'utils/constants';
 const withAriaLive = word => <span aria-live="polite">{word}</span>;
 const defaultScoreWords = ['weak', 'weak', 'okay', 'good', 'strong'];
 
+const useStyles = makeStyles({
+  div: {
+    '& > div': {
+      '& > div': {
+        height: '4px !important',
+        borderRadius: 3,
+      },
+    },
+  },
+});
+
 const PasswordStrengthBar = ({ userInputs, password }) => {
   const theme = useTheme();
+  const classes = useStyles();
+
   return (
     <ExternalPasswordStrengthBar
       password={password}
       minLength={MIN_PASSWORD_LENGTH}
       userInputs={userInputs}
-      scoreWordStyle={{ color: theme.palette.text.secondary }}
       scoreWords={defaultScoreWords.map(withAriaLive)}
       shortScoreWord={withAriaLive('too short')}
+      scoreWordStyle={{ color: theme.palette.text.primary }}
+      barColors={[theme.palette.text.primary, '#EF4836', '#5900FF', '#2F75D0', '#01892A']}
+      className={classes.div}
     />
   );
 };
